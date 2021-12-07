2adc74a6e7a708987157ed06a765d23d4b59e1fa
Meera Patel <patelmeeray@gmail.com>
2021-12-07 14:48:57 -0500

    bug fix: I/O error by running function in PLR without creating connection object

--- inst/sql/postprocessing/rxnorm_concept_update.sql
+++ inst/sql/postprocessing/rxnorm_concept_update.sql
@@ -1,6 +1,9 @@
 CREATE OR REPLACE FUNCTION setup_rxnorm_concept_update() RETURNS void AS '
 library(setupRxNorm)
-process_rxnorm_concept_update()
+library(pg13)
+conn <- local_connect()
+process_rxnorm_concept_update(conn = conn)
+dc(conn = conn)
 ' LANGUAGE plr;
 
 
