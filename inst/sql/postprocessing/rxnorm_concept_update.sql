da2837e76d4829f0b0236257a36ce7effd454bf4
Meera <mpatel@concertai.com>
2022-01-04 21:29:54 -0500

    Replace SQL for rxnorm_concept_update to function in run_postprocessing

--- inst/sql/postprocessing/rxnorm_concept_update.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION setup_rxnorm_concept_update() RETURNS void AS '
-library(setupRxNorm)
-process_rxnorm_concept_update()
-' LANGUAGE plr;
-
-
-SELECT setup_rxnorm_concept_update();
-
