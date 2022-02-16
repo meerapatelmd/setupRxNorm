b9c22f0f52b4e15f39bcba9309f489b9c5582d13
Meera Patel <patelmeeray@gmail.com>
2022-02-15 20:19:44 -0500

    update RxClass API functions

--- R/process_rxclass_api.R
+++ R/process_rxclass_api.R
@@ -290,8 +290,19 @@ class_types <-
 
 for (class_type in class_types) {
 
-  dir <- file.path(getwd(), "inst", "csv", class_type)
-  cave::dir.create_path(dir)
+  version_key <- get_rxnav_api_version()
+  path_vctr   <- c(getwd(), "inst", "RxClass API", version_key$version, "csv", class_type)
+
+  for (i in 1:length(path_vctr)) {
+
+    dir <- paste(path_vctr[1:i], collapse = .Platform$file.sep)
+
+    if (!dir.exists(dir)) {
+
+      dir.create(dir)
+    }
+
+  }
 
   class_type_node_csv <-
     file.path(dir, "node.csv")
