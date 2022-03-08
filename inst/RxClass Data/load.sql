65bdf8f7346001aa2d8405bdb9b95e0768f79b58
Meera <mpatel@concertai.com>
2022-03-07 23:56:14 -0500

    bug fixes: dedupe RxClass Concept and fix lookup

--- inst/RxClass Data/load.sql
+++ inst/RxClass Data/load.sql
@@ -12,6 +12,21 @@ CREATE TABLE tmp_lookup (
 COPY tmp_lookup FROM '{lookup_csv}'  CSV HEADER QUOTE '"';
 
 
+DROP TABLE IF EXISTS tmp_concept;
+
+CREATE TABLE tmp_concept (
+  concept_code varchar(25) NOT NULL,
+  concept_name varchar(255) NOT NULL,
+  class_type varchar(10) NOT NULL,
+  concept_class_id varchar(25) NULL,
+  standard_concept varchar(1) NULL,
+  vocabulary_id varchar(20) NOT NULL
+)
+;
+
+COPY tmp_concept FROM '{concept_csv}' CSV HEADER NULL AS 'NA' QUOTE '"';
+
+
 DROP TABLE IF EXISTS concept;
 
 CREATE TABLE concept (
@@ -24,7 +39,38 @@ CREATE TABLE concept (
 )
 ;
 
-COPY concept FROM '{concept_csv}' CSV HEADER NULL AS 'NA' QUOTE '"';
+WITH dupes AS (
+select
+  c2.*
+from tmp_concept c
+inner join tmp_concept c2
+ON
+  c2.concept_code = c.concept_code
+  AND c2.concept_name = c.concept_name
+where
+  c.vocabulary_id = 'RxNorm'
+  AND c.class_type = c2.class_type
+  AND c.vocabulary_id <> c2.vocabulary_id
+)
+
+INSERT INTO concept
+SELECT DISTINCT
+  c.concept_code,
+  c.concept_name,
+  c.class_type,
+  c.concept_class_id,
+  c.standard_concept,
+  c.vocabulary_id
+FROM tmp_concept c
+LEFT JOIN dupes d
+ON
+  c.concept_code = d.concept_code
+  AND c.concept_name = d.concept_name
+  AND c.class_type = d.class_type
+  AND c.concept_class_id = d.concept_class_id
+  AND c.vocabulary_id = d.vocabulary_id
+WHERE d.concept_code IS NULL
+;
 
 
 DROP TABLE IF EXISTS concept_synonym;
@@ -72,42 +118,44 @@ CREATE TABLE lookup (
   version_class_type varchar(1),
   version_rela_sources varchar(1),
   vocabulary_id varchar(20),
-  relationship_source varchar(25),
   subsumes_ct integer,
   mapping_ct integer
 )
 ;
 
 WITH c AS (
-SELECT c.vocabulary_id, cr.relationship_source, COUNT(*) AS subsumes_ct
+SELECT c.class_type, c.vocabulary_id, cr.relationship_source, COUNT(*) AS subsumes_ct
 FROM concept c
 INNER JOIN (SELECT * FROM concept_relationship WHERE relationship_id NOT IN ('Mapped from', 'Maps to')) cr
 ON cr.concept_code_1 = c.concept_code
-GROUP BY c.vocabulary_id, cr.relationship_source
+GROUP BY c.class_type, c.vocabulary_id, cr.relationship_source
 ),
 r AS (
-SELECT c.vocabulary_id, cr.relationship_source, COUNT(*) AS mapped_ct
+SELECT c.class_type, c.vocabulary_id, cr.relationship_source, COUNT(*) AS mapped_ct
 FROM concept c
 INNER JOIN (SELECT * FROM concept_relationship WHERE relationship_id IN ('Mapped from', 'Maps to')) cr
 ON cr.concept_code_1 = c.concept_code
-GROUP BY c.vocabulary_id, cr.relationship_source
+GROUP BY c.class_type, c.vocabulary_id, cr.relationship_source
 ),
 fin AS (
 SELECT
   tmp_lookup.*,
-  c.*,
+  c.vocabulary_id,
+  c.subsumes_ct,
   r.mapped_ct
 FROM tmp_lookup
 LEFT JOIN c
-ON c.vocabulary_id = tmp_lookup.class_type AND c.relationship_source = tmp_lookup.rela_sources
+ON c.class_type = tmp_lookup.class_type AND c.relationship_source = tmp_lookup.rela_sources
 LEFT JOIN r
-ON r.vocabulary_id = c.vocabulary_id AND r.relationship_source = c.relationship_source
+ON r.class_type = c.class_type AND r.relationship_source = c.relationship_source AND r.vocabulary_id = c.vocabulary_id
 )
 
 INSERT INTO lookup SELECT * FROM fin ORDER BY class_type, rela_sources
 ;
 
+
 DROP TABLE tmp_lookup;
+DROP TABLE tmp_concept;
 
 
 CREATE TABLE IF NOT EXISTS {log_schema}.{log_table_name} (
