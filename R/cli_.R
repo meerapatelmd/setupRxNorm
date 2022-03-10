0ed8fda1026de0a9a9e108c1a77688e05bad4ae2
Meera Patel <patelmeeray@gmail.com>
2022-03-10 13:58:22 -0500

    add cli utils

---
+++ R/cli_.R
@@ -0,0 +1,53 @@
+cli_file_missing <-
+  function(file_path) {
+
+    console_file <-
+    stringr::str_replace(file_path,
+                         pattern = "(^.*?/)(inst.*?)",
+                         replacement = "\\2")
+
+
+   cli::cli_text("[{as.character(Sys.time())}] {cli::col_red(cli::style_bold(cli::symbol$cross))} {.file {console_file}} ")
+
+
+  }
+
+
+cli_missing_file_written <-
+  function(file_path) {
+
+console_file <-
+  stringr::str_replace(file_path,
+                       pattern = "(^.*?/)(inst.*?)",
+                       replacement = "\\2")
+
+
+cli::cli_text("[{as.character(Sys.time())}] {cli::col_red(cli::style_bold(cli::symbol$cross))} {cli::style_bold(cli::symbol$arrow_right)} {cli::col_green(cli::style_bold(cli::symbol$tick))} {.file {console_file}} ")
+
+
+}
+
+
+cli_file_exists <-
+  function(file_path) {
+
+    console_file <-
+      stringr::str_replace(file_path,
+                           pattern = "(^.*?/)(inst.*?)",
+                           replacement = "\\2")
+
+
+    cli::cli_text("[{as.character(Sys.time())}] {cli::col_green(cli::style_bold(cli::symbol$tick))} {.file {console_file}} ")
+
+
+  }
+
+
+cli_message <-
+  function(msg) {
+
+      cli::cli_text("[{as.character(Sys.time())}] {msg} ")
+
+
+  }
+
